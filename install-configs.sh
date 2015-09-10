eb1f292109d0fceb675daac63eeb222c97cef26c
ajream <jijin.huang2012@gmail.com>
2015-09-10 22:51:47 +0800

    added git configs

--- install-configs.sh
+++ install-configs.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 echo "### 开始配置环境 ###"
 
-hash git >/dev/null 2>&1 || { echo "!! git not installed, cannot continue"; exit 2; }
+# 判断 git 命令是否存在
+hash git >/dev/null 2>&1 || { echo "require git but it's not installed."; exit 2; }
+
+if [ -f ~/.gitconfig ] ; then
+  echo "~/.gitconfig already exists, get the existing username and email first"
+  git_username=$(git config --global user.name)
+  git_useremail=$(git config --global user.email)
+  echo "user = $git_username"
+  echo "email = $git_useremail"
+fi
+
+echo "\ncopying .gitconfig..."
+cp -f git/my.gitconfig ~/.gitconfig || { echo "!! failed to cp gitconfig"; exit 2; }
+echo "done"
+
+# 恢复 user.name user.email 的值
+if [ ${git_username:+x} ] ; then
+	git config --global user.name "$git_username"
+  echo "set back git username to $git_username"
+fi
+
+if [ ${git_useremail:+x} ] ; then
+	git config --global user.email "$git_useremail"
+  echo "set back git email to $git_useremail"
+fi
 
 echo "\n复制 oh-my-zsh 主题和个人 zsh 配置"
 cp -f zsh/amuse.zsh-theme ~/.oh-my-zsh/themes/ || { echo "!! failed to cp amuse.zsh-theme"; exit 2; }
